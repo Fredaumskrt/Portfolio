30db27bb8f072255c0e35834e2dd3ebeebe1b34d
Frederico Malaquias <fmacaldeira@sga.pucminas.br>
2025-05-13 11:41:26 +0100

    emailJs

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -8,8 +8,26 @@ import FacebookIcon from '../../assets/facebook-icon.png'
 import TwitterIcon from '../../assets/twitter.png'
 import youtubeIcon from '../../assets/youtube.png'
 import InstagramIcon from '../../assets/instagram.png'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef;
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_b7kyl7s', 'YOUR_TEMPLATE_ID', form.current, {
+                publicKey: 'YOUR_PUBLIC_KEY',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
         <section id="contactPage">
             <div id='clients'>
@@ -28,16 +46,16 @@ const Contact = () => {
             <div id='contact'>
                 <h1 className="contacePageTitle"> Contact Me</h1>
                 <span className='contactDesc'>Please fill out the form below to discuss any work</span>
-                <form className='contactForm'>
-                    <input type = "text" className='name' placeholder='Your Name'/>
-                    <input type = "email" className='email' placeholder='Your Email'/>
-                    <textarea className = "msg" rows = "5" placeholder='Your Message'></textarea>
-                    <button type='submit' value = 'Send' className='submitBtn'>Submit</button>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type="text" className='name' placeholder='Your Name' />
+                    <input type="email" className='email' placeholder='Your Email' />
+                    <textarea className="msg" rows="5" placeholder='Your Message'></textarea>
+                    <button type='submit' value='Send' className='submitBtn'>Submit</button>
                     <div className='links'>
-                        <img src = {FacebookIcon} alt ="Facebook" className='link'/>
-                        <img src = {TwitterIcon} alt ="Twitter" className='link'/>
-                        <img src = {youtubeIcon} alt ="Youtube" className='link'/>
-                        <img src = {InstagramIcon} alt ="Instagram" className='link'/>
+                        <img src={FacebookIcon} alt="Facebook" className='link' />
+                        <img src={TwitterIcon} alt="Twitter" className='link' />
+                        <img src={youtubeIcon} alt="Youtube" className='link' />
+                        <img src={InstagramIcon} alt="Instagram" className='link' />
                     </div>
                 </form>
             </div>
